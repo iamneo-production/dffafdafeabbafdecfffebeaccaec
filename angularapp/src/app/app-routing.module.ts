148140e619d6ba0c65dadf8093611969a4d0132d
Iamneo <techadmin@iamneo.ai>
2023-08-18 05:43:32 +0000

    Backup Commit

--- angularapp/src/app/app-routing.module.ts
+++ angularapp/src/app/app-routing.module.ts
@@ -7,7 +7,6 @@ import { OrganizerComponent } from './organizer/organizer.component';
 import { LoginComponent } from './login/login.component';
 import { ErrorComponent } from './error/error.component';
 import { RegistrationComponent } from './registration/registration.component';
-import { NgForm } from '@angular/forms';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
@@ -21,6 +20,6 @@ const routes: Routes = [
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule, NgForm]
+  exports: [RouterModule]
 })
 export class AppRoutingModule { }
